56f835ecce24cd5971ee6b13df4630e7f8c8d4fa
JamesVorder <jamesvorder@gmail.com>
2017-08-13 18:43:48 -0500

    Added error handling for empty case and multiple sentences case.  Renamed template file.

--- analyze_sentence.py
+++ analyze_sentence.py
@@ -1,9 +1,14 @@
 import json
 from vaderSentiment import vaderSentiment as vs
+import nltk.tokenize
 
 sa = vs.SentimentIntensityAnalyzer()
 
 def lambda_handler(event, context):
     sentence = event["sentence"]
+    if sentence == '':
+    	raise Exception("Bad request -- Sentence cannot be null.")
+    if len(nltk.sent_tokenize(sentence)) > 1:
+    	raise Exception("Bad request -- Only one sentence per request.")
     sentiment = sa.polarity_scores(sentence)
     return sentiment
